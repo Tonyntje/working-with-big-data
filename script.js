5df9ab0f4adc7e6a98d98e97e2620be9fa8ce74d
Tony Nieuwenhuijs <tony_nieuwenhuijs@live.nl>
2022-04-28 23:28:32 +0200

    Finished assignment 6/6 with recursive functions :D

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// Short function to get the country list sorted uniquely. This was easy.
 const countryList = () => {
     const countries = randomPersonData
     .map(person => person.region)
@@ -8,6 +8,8 @@ const countryList = () => {
     createListArray(filtered);
 }
 
+// This was reasonable challenging. Specially the capricorn part.
+// But i could reuse this function later in step 6.
 const capricornWoman = () => {
     const parent = document.createElement('ul')
     const result = document.getElementById('result');
@@ -28,6 +30,8 @@ const capricornWoman = () => {
     result.append(parent);
 }
 
+// Medium sized function to get the amount of poeple per country. 
+// This was easy and could be written better
 const oldCreditCards = () => {
     const parent = document.createElement('ul')
     const result = document.getElementById('result');
@@ -56,6 +60,8 @@ const oldCreditCards = () => {
     result.append(parent);
 }
 
+// Medium sized function to get the amount of poeple per country. 
+// This was easy and could be written better
 const mostPeople = () => {
     const parent = document.createElement('ul')
     const result = document.getElementById('result');
@@ -81,6 +87,7 @@ const mostPeople = () => {
     })
 }
 
+// Short function to get the average Age. This was easy.
 const averageAge = () => {
     const result = document.getElementById('result');
     result.innerHTML = ''
@@ -100,6 +107,7 @@ const averageAge = () => {
     result.append(extraResults);
 }
 
+// SShort function to support the above
 const getAverageAge = (e) => {
     const results = document.getElementById('extra-results')
     const buttonValue = e.target.value;
@@ -108,4 +116,79 @@ const getAverageAge = (e) => {
     results.innerHTML = `The average age in ${buttonValue} is ${getAverageAgeFrom}`
 };
 
-// matchMaking()
+const matchMaking = () => {
+    // Initiate start and basics of app
+    const result = document.getElementById('result');
+    const layer = document.createElement('div')
+    let id = 1;
+    layer.id = 'gridify'
+    result.innerHTML = ''
+    result.append(layer)
+
+    // Sort all this junk
+    const sorted = randomPersonData
+        .map(person => {
+            const sign = findSign(new Date(person.birthday.mdy))
+            return {name: person.name, photo: person.photo, country: person.region, age: person.age, zodiac_sign: sign }
+        })
+        .sort((a, b) => a.name.localeCompare(b.name)).filter(person => person.age > 23 && person.name != '')
+
+    // For Each filtered mapped & sorted person perform a recursive function. Too much noise otherwise.
+    sorted.forEach(element => placeholder(element, id++, sorted, layer)) // Found id++ out by trying
+}
+
+function placeholder(element, id, sorted, layer) {
+    // Create Base Elements
+        const card = document.createElement('div')
+        const title = document.createElement('p')
+        const age = document.createElement('p')
+        const zodiacsign = document.createElement('p')
+        const region = document.createElement('p')
+        const match = document.createElement('button')
+        const image = document.createElement('img')
+    // Assign properties to Elements
+        image.src = element.photo
+        title.innerHTML = `${element.name}`
+        age.innerHTML = `${element.age}`
+        zodiacsign.innerHTML = `${element.zodiac_sign}`
+        region.innerHTML = `${element.country}`
+        match.innerHTML  = `Find match`
+    // Append elements to card (loopable via array?)
+        card.append(image)
+        card.append(title)
+        card.append(age)
+        card.append(zodiacsign)
+        card.append(region)
+        card.append(match)
+        card.id = id;
+        match.value = id;
+    // Add eventlistener to button
+    match.addEventListener('click', (e) => {
+        let id = 2;
+        // Const everyhting usefull
+        const gridi = document.getElementById('gridify')
+        const allMatches = sorted.filter(person => person.zodiac_sign === element.zodiac_sign && person.name != element.name)
+        const mainMatch = (document.getElementById(e.target.value))
+        const availableMatches = document.createElement('div')
+        const matchHeader = document.createElement('h1')
+
+        // Append a shitload with eachother
+        matchHeader.innerHTML = `Matches for ${element.name}`
+        availableMatches.id = 'matches'
+        gridi.innerHTML = ''
+        layer.innerHTML = ''
+        gridi.append(mainMatch)
+        gridi.append(matchHeader)
+        result.append(availableMatches)
+
+        // OMG! It's recursive :,)!
+        // But useless :,(!
+        // Unless >:)...
+        // Need to recurse the function to all matches. Pretty clever for future features
+        allMatches.forEach(match => {
+            placeholder(match, id, sorted, availableMatches)
+            id++
+        });
+    })
+    layer.append(card)
+}
